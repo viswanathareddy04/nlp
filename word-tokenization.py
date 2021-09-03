3f0673f14b571b1c74a76f7285334650a84b3e72
Reddy1990 <Reddy1990@users.noreply.github.com>
2021-09-03 07:27:04 +0530

    word tokenization

---
+++ word-tokenization.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  3 07:24:09 2021
+
+@author: injav
+"""
+
+import nltk.tokenize
+
+file_contents = open(r'english-content.txt', 'r').read()
+word_tokens = nltk.word_tokenize(file_contents)
+print(word_tokens)
