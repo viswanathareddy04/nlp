2e97c6c7a66d4f624d24f0462191d0085a83b7b4
Reddy1990 <Reddy1990@users.noreply.github.com>
2021-09-03 16:22:33 +0530

    Stemming

--- non-english-content-tokenization.py
+++ non-english-content-tokenization.py
@@ -7,4 +7,4 @@ Created on Fri Sep  3 07:27:48 2021
 import nltk.data
 tokenize_spanish = nltk.data.load('tokenizers/punkt/spanish.pickle')
 file_contents = open(r'spanish-content.txt',  encoding="utf8").read()
-print(tokenize_spanish.tokenize(file_contents)[0])
+tokens= tokenize_spanish.tokenize(file_contents)
---
+++ stemmer.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  3 16:03:25 2021
+
+@author: injav
+"""
+
+import nltk.tokenize
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+
+file_contents = open("english-content.txt").read()
+
+sentences = nltk.sent_tokenize(file_contents)
+stemmer =  PorterStemmer()
+for i in range(len(sentences)):
+    words = nltk.word_tokenize(sentences[i])
+    words = [stemmer.stem(word) for word in words if word not in set(stopwords.words('english'))]
+    sentences[i] = ' '.join(words)   
+    
